9cd51662733268f6f752588bb1582adfb9450e14
Lucas Daniel Lapp <85656959+LucasDanielLapp@users.noreply.github.com>
2023-12-09 23:08:52 -0300

    Primera version del script para linux
    Ya puedes crear la estructura bàsica de un archivo .conf en sistes-available , pero no puedes añadir aùn un puerto a la direciòn /etc/hosts,tampoco puedes reiniciar automaticamente . Eso en proximas versiones.

---
+++ apache.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+#Esto es parte de un queño proyecto que estoy haciendo para automatizar la creaciones de documentos para el localhost
+
+
+#falta :
+#No toca la configuracion de (etc/hosts) necesario para activar un puerto
+
+#No se reinicia automaticamente
+
+
+
+
+
+#Code
+
+#Ruta
+ruta="/etc/apache2/sites-available/"
+
+
+echo "Name of new fill of configuration web"
+
+read nameConf #nombre del archivo de configuracion
+
+ # Verifica si el archivo ya existe
+if [ -f "$ruta$nameConf" ]; 
+then 
+  echo "El archivo $nameConf ya existe en la ruta $ruta"
+else
+  #Crea el archivo en la ruta especificada
+  sudo touch "$ruta$nameConf.conf"
+  echo "Archivo $nameConf creado en la ruta $ruta"
+  #Usamos sudo tee -a para otorgar permiso de administrador y poder escribir
+  echo "<VirtualHost *:80>" | sudo tee -a "$ruta$nameConf.conf" #Structur basic
+  #Estandar virtualHost
+  echo "Nombre de la web(ServerName)"
+  read ServerName #here debes decirle el nombre de tu web
+  echo "       ServerName $ServerName.es" | sudo tee -a "$ruta$nameConf.conf" 
+  echo "Name Fill donde esta el documento web(ServerAdmin)"  
+  read ServerAdmin
+  echo "       ServerAdmin root@$ServerAdmin.es" | sudo tee -a "$ruta$nameConf.conf" 
+  #Por temas de practisidad el serverAlias siempre sera igual a Servernamber , lo ùnico que agregamos son los terminos (www) and (.es)
+  echo "       ServerAlias www.$ServerName.es" | sudo tee -a $ruta$nameConf.conf 
+ #DocumentRoot
+ #Normalmente /var/www/MyFill 
+ #Lo que are dado que todas mis web comparten la direcion /var/www/ are que como en ServerAdmin ya puse la carpeta en que estare trabajando se autocomplete. Se puede modificar el la line posterior cambiando la ruta(/var/www/) antes de la variable $ServerAdmin.
+ echo "       DocumentRoot /var/www/$ServerAdmin" | sudo tee -a $ruta$nameConf.conf    
+
+ #Como el documento a utilizar puede ser html o php no especificare el formato automaticamente , lo tendra que hacer el usuario.
+
+ echo "archivo index(DirectoryIndex)"
+ read DirectoryIndex
+ echo "       DirectoryIndex $DirectoryIndex" | sudo tee -a $ruta$nameConf.conf
+ echo "</VirtualHost>" | sudo tee -a $ruta$nameConf.conf
+
+ #Cerramos la estructura bàsica de un virtualHost
+fi  
+
